58a03e3a5681a3e3239b41ee73f04dd7e2e0b014
Miro Wengner <miro@robo4j.io>
2018-12-18 17:48:14 +0100

    spring-autoconfigure docu update

--- opentracing-jfr-tracer-spring-autoconfigure/src/main/java/io/opentracing/contrib/jfrtracer/spring/autoconfigure/JfrTracerBeanPostProcessor.java
+++ opentracing-jfr-tracer-spring-autoconfigure/src/main/java/io/opentracing/contrib/jfrtracer/spring/autoconfigure/JfrTracerBeanPostProcessor.java
@@ -8,10 +8,28 @@ import org.springframework.context.annotation.Configuration;
 import io.opentracing.Tracer;
 import io.opentracing.contrib.jfrtracer.JfrTracerFactory;
 
+/**
+ * BeanPostProcessor {@link BeanPostProcessor} wraps available {@link Tracer} into jfr
+ * {@link io.opentracing.contrib.jfrtracer.impl.wrapper.TracerWrapper}. Configuration is enabled by
+ * default and can be disabled by setting up environment variable opentracing.jfr-tracer.enabled to
+ * false
+ */
 @Configuration
 @ConditionalOnProperty(value = "opentracing.jfr-tracer.enabled", havingValue = "true", matchIfMissing = true)
 public class JfrTracerBeanPostProcessor implements BeanPostProcessor {
 
+	/**
+	 * wrapping new instantiated tracer bean to JFR Tracer see
+	 * {@link BeanPostProcessor#postProcessAfterInitialization(Object, String)}
+	 *
+	 * @param bean
+	 *            available bean
+	 * @param beanName
+	 *            bean name
+	 * @return wrapped tracer or bean
+	 * @throws BeansException
+	 *             bean exception
+	 */
 	@Override
 	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
 		if (bean instanceof Tracer) {
@@ -20,6 +38,17 @@ public class JfrTracerBeanPostProcessor implements BeanPostProcessor {
 		return bean;
 	}
 
+	/**
+	 * see {@link BeanPostProcessor#postProcessBeforeInitialization(Object, String)}
+	 * 
+	 * @param bean
+	 *            bean
+	 * @param beanName
+	 *            bean name
+	 * @return bean
+	 * @throws BeansException
+	 *             bean exception
+	 */
 	@Override
 	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
 		return bean;
